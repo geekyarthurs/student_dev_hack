222673090af5d94a358f1a57d6c9a4912073ef4d
Mahesh C. Regmi <geekyarthurs@gmail.com>
2019-12-07 14:55:25 +0545

    initial

---
+++ ai_api.py
@@ -0,0 +1,24 @@
+import json
+from ibm_watson import VisualRecognitionV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import sys
+authenticator = IAMAuthenticator('t-HtOTTNnoEIQvjUCbfW6oI2Mb5SI3XTlAmbdPpkFOD8')
+visual_recognition = VisualRecognitionV3(
+    version='2018-03-19',
+    authenticator=authenticator
+)
+  
+visual_recognition.set_service_url('https://gateway.watsonplatform.net/visual-recognition/api')
+def detect_image(imagePath):  
+    with open(imagePath, 'rb') as images_file:
+     classes = visual_recognition.classify(
+            images_file=images_file,
+         threshold='0.3',
+         classifier_ids='food'
+
+        ).get_result()
+  
+      #["images"][0]["classifiers"][0]["classes"]
+  
+    return classes
+                         
--- app.py
+++ app.py
@@ -1,45 +1,93 @@
 from flask import Flask, render_template, url_for, flash, redirect, session, request
 import math
 from flask_pymongo import PyMongo
-app = Flask(__name__)
+from bson.objectid import ObjectId
+from ai_api import detect_image
+app = Flask(__name__, static_folder='static')
 app.secret_key = 'prapannaisniceguyhehasfivegirlfriends'
-app.config['MONGO_URI'] =  "mongodb://localhost:27017/student_dev_hack"
+app.config['MONGO_URI'] = "mongodb://localhost:27017/student_dev_hack"
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def landing_page():
-    users = mongo.db.users.find()
-    print(users[0])
-    # return "OK" 
+    return render_template("index.html")
 
 
+@app.route("/detect", methods=["GET", "POST"])
+def detect():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(f.filename)
+        filename = f.filename
+        detectedClasses = detect_image(filename)
+        image_classified = detectedClasses["images"][0]["classifiers"][0]["classes"]
+        images = [obj["class"] for obj in image_classified]
+        # print(images)
 
+        foods = {"bread": {
+            "fullname": "Selroti",
+            "ingredients": ["rice flour", "water", "sugar", "butter"],
+            "types": ["Indian", "Nepali", "Newari"],
+            "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
+            "price": "Rs. 100",
+            "energy": "60 kcal",
+            "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
 
+        }, "flour": {
+            "fullname": "Selroti",
+            "ingredients": ["rice flour", "water", "sugar", "butter"],
+            "types": ["Indian", "Nepali", "Newari"],
+            "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
+            "price": "Rs. 100",
+            "energy": "60 kcal",
+            "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
 
-@app.route("/login", methods= ["POST"])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        users = mongo.db.users.find_one({"email": email, "password": password})
-        if users:
-            session['username'] = users['username']
-            session["_id"] = users["_id"]
-            return redirect("/dashboard")
+        }, "wheat flour": {
+            "fullname": "Selroti",
+            "ingredients": ["rice flour", "water", "sugar", "butter"],
+            "types": ["Indian", "Nepali", "Newari"],
+            "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
+            "price": "Rs. 100",
+            "energy": "60 kcal",
+            "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
+
+        }, "wontons": {
+            "fullname": "MOMO",
+            "ingredients": ["white-flour", "meat", "vegetable", "tomato chutney", "tomato soup", "soyabean"],
+            "variations": ["steam-momo", " Kothey momo", "C-momo", "Fry-momo", "Open-momo", "fried momo"],
+            "energy": "400 kcal",
+            "imageUrl": "https://checkplease.wttw.com/sites/default/files/momo.jpg",
+            "price": "Rs. 150"
+        }, "dumplings": {
+            "fullname": "MOMO",
+            "ingredients": ["white-flour", "meat", "vegetable", "tomato chutney", "tomato soup", "soyabean"],
+            "variations": ["steam-momo", " Kothey momo", "C-momo", "Fry-momo", "Open-momo", "fried momo"],
+            "energy": "400 kcal",
+            "imageUrl": "https://checkplease.wttw.com/sites/default/files/momo.jpg",
+            "price": "Rs. 150"
+        }, "samosa": {
+            "fullname": "Samosa",
+            "ingredients": ["white-flour", "vegetable", "peas", "peanuts", "soyabean", "potato"],
+            "variations": ["indian", "nepali", "chaat"],
+            "price": "Rs. 100",
+            "energy": "60 kcal",
+            "imageUrl": "https://m.recipes.timesofindia.com/recipes/samosa/photo/61050397.cms"
+        }}
+
+        # DalBhat = {
+        #     "fullname": "Dalbat",
+        #     "ingredients": ["Rice", "vegetable", "lentils", "chutney"],
+        #     "types": ["Nepali", "Thakali", "Indian", "Srilanka"],
+        #     "energy": "800 kcal"
+        #
+
+    for image in images:
+        
+        if image in foods.keys():
+            return render_template("foods.html", foods=foods[image])
         else:
-            error = "Invalid Username or Password"
-            return "INVALID"
+            return render_template("index.html", error="error")
 
 
-
-@app.route("/dashboard")
-def home():
-    id = session["_id"]
-    user = mongo.db.users.find_one({"_id": "id"}
-    return render_template("dashboard.html")
-
-
-
-
-app.run()
+app.run(debug=True)
