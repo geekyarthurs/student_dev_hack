50d97d20630ace6cc8acde8821b7f9aa4af074e0
Mahesh C. Regmi <geekyarthurs@gmail.com>
2019-12-11 22:10:48 +0545

    final

--- app.py
+++ app.py
@@ -16,78 +16,85 @@ def landing_page():
 
 @app.route("/detect", methods=["GET", "POST"])
 def detect():
-    if request.method == "POST":
-        f = request.files['file']
-        f.save(f.filename)
-        filename = f.filename
-        detectedClasses = detect_image(filename)
-        image_classified = detectedClasses["images"][0]["classifiers"][0]["classes"]
-        images = [obj["class"] for obj in image_classified]
-        # print(images)
+    try:
 
-        foods = {"bread": {
-            "fullname": "Selroti",
-            "ingredients": ["rice flour", "water", "sugar", "butter"],
-            "types": ["Indian", "Nepali", "Newari"],
-            "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
-            "price": "Rs. 100",
-            "energy": "60 kcal",
-            "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
+        if request.method == "POST":
 
-        }, "flour": {
-            "fullname": "Selroti",
-            "ingredients": ["rice flour", "water", "sugar", "butter"],
-            "types": ["Indian", "Nepali", "Newari"],
-            "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
-            "price": "Rs. 100",
-            "energy": "60 kcal",
-            "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
+            
+            
+            f = request.files['file']
+            f.save(f.filename)
+            filename = f.filename
+            detectedClasses = detect_image(filename)
+            image_classified = detectedClasses["images"][0]["classifiers"][0]["classes"]
+            images = [obj["class"] for obj in image_classified]
+            # print(images)
 
-        }, "wheat flour": {
-            "fullname": "Selroti",
-            "ingredients": ["rice flour", "water", "sugar", "butter"],
-            "types": ["Indian", "Nepali", "Newari"],
-            "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
-            "price": "Rs. 100",
-            "energy": "60 kcal",
-            "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
+            foods = {"bread": {
+                "fullname": "Selroti",
+                "ingredients": ["rice flour", "water", "sugar", "butter"],
+                "types": ["Indian", "Nepali", "Newari"],
+                "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
+                "price": "Rs. 100",
+                "energy": "60 kcal",
+                "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
 
-        }, "wontons": {
-            "fullname": "MOMO",
-            "ingredients": ["white-flour", "meat", "vegetable", "tomato chutney", "tomato soup", "soyabean"],
-            "variations": ["steam-momo", " Kothey momo", "C-momo", "Fry-momo", "Open-momo", "fried momo"],
-            "energy": "400 kcal",
-            "imageUrl": "https://checkplease.wttw.com/sites/default/files/momo.jpg",
-            "price": "Rs. 150"
-        }, "dumplings": {
-            "fullname": "MOMO",
-            "ingredients": ["white-flour", "meat", "vegetable", "tomato chutney", "tomato soup", "soyabean"],
-            "variations": ["steam-momo", " Kothey momo", "C-momo", "Fry-momo", "Open-momo", "fried momo"],
-            "energy": "400 kcal",
-            "imageUrl": "https://checkplease.wttw.com/sites/default/files/momo.jpg",
-            "price": "Rs. 150"
-        }, "samosa": {
-            "fullname": "Samosa",
-            "ingredients": ["white-flour", "vegetable", "peas", "peanuts", "soyabean", "potato"],
-            "variations": ["indian", "nepali", "chaat"],
-            "price": "Rs. 100",
-            "energy": "60 kcal",
-            "imageUrl": "https://m.recipes.timesofindia.com/recipes/samosa/photo/61050397.cms"
-        }}
+            }, "flour": {
+                "fullname": "Selroti",
+                "ingredients": ["rice flour", "water", "sugar", "butter"],
+                "types": ["Indian", "Nepali", "Newari"],
+                "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
+                "price": "Rs. 100",
+                "energy": "60 kcal",
+                "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
 
-        # DalBhat = {
-        #     "fullname": "Dalbat",
-        #     "ingredients": ["Rice", "vegetable", "lentils", "chutney"],
-        #     "types": ["Nepali", "Thakali", "Indian", "Srilanka"],
-        #     "energy": "800 kcal"
-        #
+            }, "wheat flour": {
+                "fullname": "Selroti",
+                "ingredients": ["rice flour", "water", "sugar", "butter"],
+                "types": ["Indian", "Nepali", "Newari"],
+                "variations": ["SelRoti Tarkari", "Selroti Chiya", "Selroti Dahi"],
+                "price": "Rs. 100",
+                "energy": "60 kcal",
+                "imageUrl": "https://i.ytimg.com/vi/QvQy2ifqoHU/maxresdefault.jpg"
 
-    for image in images:
-        
-        if image in foods.keys():
-            return render_template("foods.html", foods=foods[image])
-        else:
-            return render_template("index.html", error="error")
+            }, "wontons": {
+                "fullname": "MOMO",
+                "ingredients": ["white-flour", "meat", "vegetable", "tomato chutney", "tomato soup", "soyabean"],
+                "variations": ["steam-momo", " Kothey momo", "C-momo", "Fry-momo", "Open-momo", "fried momo"],
+                "energy": "400 kcal",
+                "imageUrl": "https://checkplease.wttw.com/sites/default/files/momo.jpg",
+                "price": "Rs. 150"
+            }, "dumplings": {
+                "fullname": "MOMO",
+                "ingredients": ["white-flour", "meat", "vegetable", "tomato chutney", "tomato soup", "soyabean"],
+                "variations": ["steam-momo", " Kothey momo", "C-momo", "Fry-momo", "Open-momo", "fried momo"],
+                "energy": "400 kcal",
+                "imageUrl": "https://checkplease.wttw.com/sites/default/files/momo.jpg",
+                "price": "Rs. 150"
+            }, "samosa": {
+                "fullname": "Samosa",
+                "ingredients": ["white-flour", "vegetable", "peas", "peanuts", "soyabean", "potato"],
+                "variations": ["indian", "nepali", "chaat"],
+                "price": "Rs. 100",
+                "energy": "60 kcal",
+                "imageUrl": "https://m.recipes.timesofindia.com/recipes/samosa/photo/61050397.cms"
+            }}
+
+            # DalBhat = {
+            #     "fullname": "Dalbat",
+            #     "ingredients": ["Rice", "vegetable", "lentils", "chutney"],
+            #     "types": ["Nepali", "Thakali", "Indian", "Srilanka"],
+            #     "energy": "800 kcal"
+            #
+
+        for image in images:
+
+            if image in foods.keys():
+                return render_template("foods.html", foods=foods[image])
+            else:
+                return render_template("index.html", error="CANNOT PROCESS!")
+    except:
+        return render_template("index.html", error="INTERNET ISSUE!")
 
 
 app.run(debug=True)
